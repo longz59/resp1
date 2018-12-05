e852637b0e18dbd697d3f553a66e50c676c72c2d
longz <lz657284496@163.com>
2018-12-05 21:26:23 +0800

    aaaaa

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -5,7 +5,11 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!1111111111111111");
-		int b = 20;
-		int a = 10 ;
+        int b = 20;
+        int a = 10 ;
+        int c = 30;
+        int d  = 10;
+        int e  = 19;
+        int f = 36;
     }
 }
